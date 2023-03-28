007f333d8ce30417a5c6bc07f30b831b043e3d10
marcus-biddle <marcusbiddle16@gmail.com>
2023-03-28 16:10:54 -0700

    sidebar styling

--- portfolio/src/components/sidebar.tsx
+++ portfolio/src/components/sidebar.tsx
@@ -15,7 +15,7 @@ const Logo = () => {
             opacity: 1
         }}
         transition={{ delay: .35 }}
-        className='text-orange-600 z-0'
+        className='z-0'
         >
             M
         </motion.div>
@@ -26,37 +26,39 @@ export default function Sidebar() {
     const { pathname } = useRouter();
     
     return (
-        <div className="flex flex-col lg: sticky lg:top-20">
+        <div className="lg:sticky lg:top-20">
             <div>
             <Logo />
             </div>
             <LayoutGroup>
-            {routes.map((route) => {
-                return (
-                    <Link
-                        key={route.path}
-                        href={route.path}
-                        className={`capitalize z-50 my-2 transition-all`}
-                        >
-                            <span className="relative">
-                                <div className={`pt-1 pl-3 ${pathname === route.path ? 'font-bold' : ''}`}>
-                                {route.name}
-                                </div>
-                                {pathname === route.path ? (
-                                    <motion.div
-                                    className='absolute z-[-1] px-9 py-4 bg-slate-300 inset-0 bg-neutral-100 rounded-lg'
-                                    layoutId="sidebar"
-                                    transition={{
-                                        type: 'spring',
-                                        stiffness: 350,
-                                        damping: 30
-                                    }}
-                                    />
-                                ): null}
-                            </span>
-                    </Link>
-                )
-            })}
+                <nav className="relative flex flex-col items-start space-x-0 space-y-3">
+                    {routes.map((route) => {
+                    return (
+                        <Link
+                            key={route.path}
+                            href={route.path}
+                            className={`capitalize z-50 transition-all`}
+                            >
+                                <span className="relative">
+                                    <div className={`pt-1 px-2 ${pathname === route.path ? 'font-bold' : 'text-neutral-500'}`}>
+                                    {route.name}
+                                    </div>
+                                    {pathname === route.path ? (
+                                        <motion.div
+                                        className='absolute z-[-1] bg-slate-300 inset-0 rounded-lg'
+                                        layoutId="sidebar"
+                                        transition={{
+                                            type: 'spring',
+                                            stiffness: 350,
+                                            damping: 30
+                                        }}
+                                        />
+                                    ): null}
+                                </span>
+                        </Link>
+                        )
+                    })}
+                </nav>
             </LayoutGroup>
         </div>
     );
